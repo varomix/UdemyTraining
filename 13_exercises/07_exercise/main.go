88b9636ccb7e07bd5dbac2c2756d18e4257d507d
varomix wey <varomix@gmail.com>
2016-01-07 00:13:45 -0700

    orrected exercise 7 to use OR instead of AND

--- 13_exercises/07_exercise/main.go
+++ 13_exercises/07_exercise/main.go
@@ -6,10 +6,10 @@ func main() {
 	result := 0
 	for i := 1; i < 1000; i++ {
 		switch {
-		case i%3 == 0 && i%5 == 0:
+		case i%3 == 0 || i%5 == 0:
 			result += i
 		}
 	}
 
-	fmt.Println("Result:", result) // Result: 33165
+	fmt.Println("Result:", result) // Result: 233168
 }
